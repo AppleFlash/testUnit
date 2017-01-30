9f22434513195e457e6bdf7e2bab9f4a47bf130a
Vladislav <sedinkin1993@gmail.com>
2017-01-30 23:46:40 +0600

    закомичу и откачусь

--- UnitTestsTests/UnitTestsTests.swift
+++ UnitTestsTests/UnitTestsTests.swift
@@ -33,6 +33,20 @@ class UnitTestsTests: XCTestCase {
         }
     }
     
+    func testPerformanceExample1() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testPerformanceExample2() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
     func testForUnit()
     {
         let vc = ViewController()
